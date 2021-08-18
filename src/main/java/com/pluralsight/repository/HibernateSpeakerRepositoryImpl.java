9703aa924f310556de2e361b93d7e7645c62e456
THIYAGU22 <thiyagarajanravi22@gmail.com>
2021-08-18 10:19:49 +0530

    Added SPEL expression

--- src/main/java/com/pluralsight/repository/HibernateSpeakerRepositoryImpl.java
+++ src/main/java/com/pluralsight/repository/HibernateSpeakerRepositoryImpl.java
@@ -2,6 +2,7 @@ package com.pluralsight.repository;
 
 import com.pluralsight.model.Speaker;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Repository;
 
 import java.util.ArrayList;
@@ -13,6 +14,9 @@ public class HibernateSpeakerRepositoryImpl implements SpeakerRepository {
 
     @Autowired
     private Calendar calendar;
+
+    @Value("#{ T(java.lang.Math).random() * 100.0 }")
+    private double seedNum;
     @Override
     public List<Speaker> findAll() {
         List<Speaker> speakers = new ArrayList<>();
@@ -20,6 +24,8 @@ public class HibernateSpeakerRepositoryImpl implements SpeakerRepository {
         Speaker speaker1 = new Speaker();
         speaker1.setFirstName("Thiyagarajan");
         speaker1.setLastName("Ravichandran");
+        speaker1.setSeedNum(seedNum);
+
 
         System.out.println("Cal : " + calendar.getTime());
         speakers.add(speaker1);
